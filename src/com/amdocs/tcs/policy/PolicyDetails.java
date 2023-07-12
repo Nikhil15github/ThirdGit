b1204abe513ce4f34f368ebba116dfbc449c180e
Nikhil <nikpawarnp2@gmail.com>
2023-07-12 12:07:14 +0530

    add Veriable

--- src/com/amdocs/tcs/policy/PolicyDetails.java
+++ src/com/amdocs/tcs/policy/PolicyDetails.java
@@ -1,5 +1,13 @@
 package com.amdocs.tcs.policy;
 
-public class PolicyDetails {
+public class PolicyDetails 
+{
 
+	
+	public static void main(String[] args) 
+	{
+		
+		int policyNumber= 76876876;
+		System.out.println(policyNumber);
+	}
 }

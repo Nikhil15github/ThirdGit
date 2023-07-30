88f3ef43050e35c30413c4e9af90e8eaf70567b2
Nikhil <nikpawarnp2@gmail.com>
2023-07-30 19:18:12 +0530

    new pakage

---
+++ src/com/amdocs/tcs/insurance/InsuranceNumber.java
@@ -0,0 +1,5 @@
+package com.amdocs.tcs.insurance;
+
+public class InsuranceNumber {
+
+}
---
+++ src/com/amdocs/tcs/policy/PolicyId.java
@@ -0,0 +1,12 @@
+package com.amdocs.tcs.policy;
+
+public class PolicyId 
+{
+
+	public static void main(String[] args) {
+		
+		int pid=90;
+		System.out.println(pid);
+	}
+	
+}
